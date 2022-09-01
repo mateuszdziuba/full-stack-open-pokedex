5fd3847625a211c9297f180aa63d307f0d3fe127
Mateusz <mdzik@DESKTOP-Q28858B.localdomain>
2022-09-01 15:26:22 +0200

    Fixed cypress linting

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    // implement node event listeners here
+    // },
   },
 });
